6fa29a0d26860ea48cfd8468617d96f25ca987f3
Nahusoft <68044306+Nahusoft@users.noreply.github.com>
2023-02-28 04:44:16 -0300

    Actualizacion
    Mejorando el codigo

--- js/form.js
+++ js/form.js
@@ -1,51 +1,44 @@
 
-
+/*
 document.addEventListener('DOMContentLoaded', function() {
 
-// Obtener todos los iconos y agregar un evento de clic a cada uno
+//* Obtener todos los iconos y agregar un evento de clic a cada uno
 var icons = document.querySelectorAll('.barra-iconos i2');
 icons.forEach(function(icon) {
   icon.addEventListener('click', function() {
-    // Obtener el ID del icono clickeado
+    //* Obtener el ID del icono clickeado
     var iconId = this.id;
-    // Mostrar una alerta con el ID del icono
+    //* Mostrar una alerta con el ID del icono
     alert(iconId);
   });
 });
-
-
 });
+
+*/
+
 
 function validarFormulario() {
     var nombre = document.getElementById("nombre").value;
     var apellido = document.getElementById("apellido").value;
     var email = document.getElementById("email").value;
     
-    // Validar que los campos no estén vacíos
+    //* Validar que los campos no estén vacíos
     if (!nombre || !apellido || !email) {
       alert("Por favor, rellena todos los campos del formulario.");
       return;
     }
     
-    // Validar formato de correo electrónico
+    //* Validar formato de correo electrónico
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (!re.test(email)) {
       alert("Por favor, ingresa un correo electrónico válido.");
       return;
     }
     
-    // Si todo está bien, enviar el formulario
+    //* Si todo está bien, enviar el formulario
     alert("Formulario enviado correctamente!");
     document.getElementById("formulario").submit();
   }
 
-// Selecciona todos los elementos de icono con la clase "icon"
-/* const icons = document.querySelectorAll(".icon");
 
- Agrega un evento "mouseover" a cada icono
-icons.forEach(icon => {
-  icon.addEventListener("mouseover", () => {
-    icon.classList.add("hover");
-  });
-}); */
 
